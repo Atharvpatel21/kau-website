ec1d70fd65a728935ebb317806087a75934bc8fe
Sandhaya Patel <56769455+Atharvpatel21@users.noreply.github.com>
2022-06-30 08:54:39 +0200

    this is a basic website home page, about page and contact page made in wordpress using html,css and javascripts
    this is my karlstads university website

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package kau-transit
+ */
+
+get_header();
+?>
+
+<!-- </div>
+</div><div class="row">
+<div class="col-md-8 col-xs-12">
+<div id="primary" class="content-area">
+	<main id="primary" class="site-main">
+
+		
+<article id="post-2" class="post-2 page type-page status-publish hentry">
+	<header class="entry-header">
+			</header>
+  -->
+	
+	<div class="entry-content">
+		
+<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile has-pale-cyan-blue-background-color has-background" style="grid-template-columns:auto 55%" id="useful-hint"><figure class="wp-block-media-text__media"><img width="150" height="150" src="http://localhost:10101/wp-content/uploads/2022/05/codingdevelopmentjslogo-150x150.png" alt="" class="wp-image-103 size-thumbnail" srcset="http://localhost:10101/wp-content/uploads/2022/05/codingdevelopmentjslogo-150x150.png 150w, http://localhost:10101/wp-content/uploads/2022/05/codingdevelopmentjslogo-300x300.png 300w, http://localhost:10101/wp-content/uploads/2022/05/codingdevelopmentjslogo.png 512w" sizes="(max-width: 150px) 100vw, 150px" /></figure><div class="wp-block-media-text__content">
+<h1 class="P1cover has-vivid-cyan-blue-color has-text-color" id="useful-hint">Welcome to Transit training and inspiration site for sustainable mobility!</h1>
+</div></div>
+
+
+
+<div class="wp-block-media-text alignwide is-stacked-on-mobile has-pale-cyan-blue-background-color has-background"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content">
+<p class="has-large-font-size"><strong>Latest news</strong></p>
+
+
+
+<p class="has-text-color" style="color:#5f656b">Stay up to date with news from the project</p>
+
+
+
+<h4 class="has-text-align-left has-vivid-cyan-blue-color has-text-color">Welcome to Transit training and inspiration site for sustainable mobility!</h4>
+
+
+
+<p class="has-text-color" style="color:#40484f">NB! This is an example of a post.&nbsp; You have two options with this post: Keep – and make it your own. Start editing this post by clicking on the Edit button at the top.<a href="https://sola.kau.se/transit1/2022/04/01/welcome-to-transit-training-and-inspiration-site-for-sustainable-mobility/">Read more…</a></p>
+</div></div>
+
+
+
+<div class="wp-block-cover alignfull p2cover" style="min-height:577px" id="useful-hint"><span aria-hidden="true" class="has-background-dim-70 wp-block-cover__gradient-background has-background-dim"></span><img loading="lazy" width="2560" height="1707" class="wp-block-cover__image-background wp-image-29" alt="" src="http://localhost:10101/wp-content/uploads/2022/05/flower-scaled.jpg" style="object-position:71% 56%" data-object-fit="cover" data-object-position="71% 56%" srcset="http://localhost:10101/wp-content/uploads/2022/05/flower.jpg 2560w, http://localhost:10101/wp-content/uploads/2022/05/flower-300x200.jpg 300w, http://localhost:10101/wp-content/uploads/2022/05/flower-1024x683.jpg 1024w, http://localhost:10101/wp-content/uploads/2022/04/flower-768x512.jpg 768w, http://localhost:10101/wp-content/uploads/2022/05/flower-1536x1024.jpg 1536w, http://localhost:10101/wp-content/uploads/2022/05/flower-2048x1365.jpg 2048w" sizes="(max-width: 2560px) 100vw, 2560px" /><div class="wp-block-cover__inner-container">
+<h1 class="has-large-font-size" style=" margin: bottom 30px;font-weight:700;line: height 7px;" ><strong>Get in Touch </strong><br></h1>
+
+
+
+
+<div class="footer__social">
+	   <h4>
+                 <a href="#" class="footer__icon"><i class='bx bxl-facebook'> </i></a>
+Find us at the office</h4>
+
+
+
+<p class="p1" >Universitetsgatan 2, 65188 Karlstad, Sweden</p>
+
+
+<h4 style="padding-left: 4rem;">
+     <img draggable="false" role="img" class="emoji" alt="↗" style="width: 350px;height:800px;" src="https://svgsilh.com/png-512/1682317.png "/>
+       Give us a ring or mail</h4>
+
+
+
+<p  class="p1" >Magnus Lindh<br>+46 54 700 17 90<br><a href="mailto:magnus.lindh@kau.se">magnus.lindh@kau.se</a></p>
+
+</div>
+</div>
+
+
+	</div><!-- .entry-content -->
+
+	<!-- <footer class="footer" >
+  
+
+  <p class="footer__title">Sandhaya Patel</p>
+  
+  <div class="footer__social">
+	
+	<a href="#" class="footer__icon"><i class='bx bxl-facebook'></i></a>
+	<a href="#" class="footer__icon"><i class='bx bxl-instagram' ></i></a>
+	<a href="#" class="footer__icon"><i class='bx bxl-twitter' ></i></a>
+	<a href="#" class= "footer__icon"><i class='bx bxl-github' ></i></a>
+  </div>
+  <p style="color:white;font-size:14px;font-style:normal;font-height:1.3;font-weight:300;" >&#169; 2020  copyright  all  right  reserved</p>
+</footer>
+
+
+  <!-- SCROLL REVEAL -->
+  <!-- <script src="https://unpkg.com/scrollreveal"></script>
+
+<script src="js/main.js"></script> --> 
+
+	</article><!-- #post-2 -->
+
+</div>
+	</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!--col-md-8 col-xs-12 -->
+<?php 
+ get_sidebar();
+ get_footer();
